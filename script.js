e10c6338bab8a7edecc9e21ba2975cafb52a07ff
manojkrishnan1 <127621202+manojkrishnan1@users.noreply.github.com>
2023-11-17 18:12:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 //your JS code here. If required.
-function functionName(){
-	alert(arguments.callee.name);
-}
-functionName();
-function Acciojob(){
-  alert(arguments.callee.name);
-}
-Acciojob();
+// function functionName(){
+// 	alert(arguments.callee.name);
+// }
+// functionName();
+// function Acciojob(){
+//   alert(arguments.callee.name);
+// }
+// Acciojob();
